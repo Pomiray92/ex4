6377facd59245bebe6ff8daf7a5b563cd0094ee3
writehereyourname <writehereyouremail>
2022-08-12 12:47:49 +0200

    ex4

---
+++ exercise.py
@@ -0,0 +1,65 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+
+
+#task1ex4
+
+# start a loop for x times
+## get number 1
+## get number 2
+### comp 1 and 2
+# >>> restart loop
+'''
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+number = input("Enter a number: ") 
+number2 = input("Enter a second number: ")
+print(not "")
+print(not "aek1")
+print(bool (""))
+'''
+
+months = ["January", "February", "March", "April", "May", "June", "July", 
+"August", "September", "October", "November", "December"]
+
+
+user = input("Input the name of Month: ")
+if user.lower() == months[0].lower():
+    print(user.capitalize(),": Number of days: 31 days")
+elif user == months[1]:
+    print("Number of days: 28 days")
+elif user == months[2]:
+    print("Number of days: 31 days")
+elif user == months[3]: 
+    print("Number of days: 30 days")
+elif user == months[4]:
+    print("Number of days: 31 days")
+elif user == months[5]:
+    print("Number of days: 31 days")
+elif user == months[6]:
+    print("Number of days: 30 days")
+elif user == months[6]:
+    print("Number of days: 30 days")
+elif user == months[7]:
+    print("Number of days: 30 days")
+elif user == months[8]:
+    print("Number of days: 31 days")
+elif user == months[9]:
+    print("Number of days: 30 days")
+elif user == months[10]:
+    print("Number of days: 31 days")
+elif user == months[11]:
+    print("Number of days: 30 days")
+else:
+    print("Check for type error")
+
+
+
+
+
+
+
+
